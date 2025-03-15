4d6b4482a11621f70bd77d1bf83549c24cb83f7e
J. Ray Stiffler <>
2025-03-15 12:26:16 -0400

    Initial project structure

---
+++ src/main/java/com/collectioniq/ciqschema/CiqSchemaApplication.java
@@ -0,0 +1,13 @@
+package com.collectioniq.ciqschema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiqSchemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiqSchemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collectioniq/ciqschema/CiqSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collectioniq.ciqschema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiqSchemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
