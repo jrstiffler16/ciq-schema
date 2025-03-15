a8350ae96c35cb02d2be54cbbd3509dd347d42a2
J. Ray Stiffler <>
2025-03-15 17:32:03 -0400

    F1-T1: extending test framework to operate against a ciqtest schema

--- src/main/resources/db/migration/V1.1__brand.sql
+++ src/main/resources/db/migration/V1.1__brand.sql
@@ -1,8 +1,15 @@
 CREATE SCHEMA IF NOT EXISTS ciq;
 
 CREATE table `brand`(
-    ${namedentity},
-    ${audit},
+    `pid` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    `name` VARCHAR(50) NOT NULL,
+    `enabled` TINYINT NOT NULL DEFAULT 1,
+    `create_datetm` DATETIME NOT NULL DEFAULT now(),
+    `last_update_datetm` DATETIME,
+    `deleted_ind` BIGINT NOT NULL DEFAULT 0,
+    `create_user` VARCHAR(50) NOT NULL,
+    `last_update_user` VARCHAR(50),
+    `delete_user` VARCHAR(50),
     UNIQUE
     (`name`,`deleted_ind`)
 );
